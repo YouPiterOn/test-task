1a74d9a942c853c177235460ad148d73604ba89e
youpiteron <oyushchenko@kse.org.ua>
2024-11-25 18:55:35 +0200

    task-3: Form to search by country implemented

--- task-3/src/App.js
+++ task-3/src/App.js
@@ -1,21 +1,35 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import universitiesClient from "./clients/universitiesClient";
 import Table from "./components/Table/Table";
 import styles from './App.module.css';
 
 const App = () => {
+  const [country, setCountry] = useState('');
   const [universities, setUniversities] = useState([]);
 
-  useEffect(() => {
-    const fetchUniversities = async () => {
-      const data = await universitiesClient.getUniversitiesByCountry('turkey');
-      setUniversities(data);
-    }
-    fetchUniversities();
-  }, [])
+  const fetchUniversities = async () => {
+    const data = await universitiesClient.getUniversitiesByCountry(country);
+    setUniversities(data);
+  };
 
-  return(
+  const reset = () => {
+    setCountry('');
+    setUniversities([]);
+  };
+
+  return (
     <div className={styles.app}>
+      <div>
+        <input className={styles.textInput}
+          type="text"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+          placeholder="Enter country name"
+        />
+        <button className={styles.button} onClick={fetchUniversities}>Submit</button>
+        <button className={styles.button} onClick={reset}>Reset</button>
+      </div>
+
       {universities.length > 0 && (
         <Table universities={universities}></Table>
       )}
